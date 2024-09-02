b1504979ca189e29bcd9d0ce77715ac87d12d02e
bcharrey <61541836+bcharrey@users.noreply.github.com>
2024-09-02 20:01:20 +0200

    created dog enemy that always run towards the player

--- Assets/Scripts/Dog.cs
+++ Assets/Scripts/Dog.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Dog : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(PlayerController.Instance.transform);
+
+        // Calculate the direction from the dog to the player
+        Vector3 direction = PlayerController.Instance.transform.position - transform.position;
+        direction.Normalize();
+
+        // Move the dog towards the player
+        transform.position += direction * Speed * Time.deltaTime;
     }
 }
