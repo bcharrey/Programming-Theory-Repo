0360d7cc099ed321ed113be50cf9d645ac34012f
bcharrey <61541836+bcharrey@users.noreply.github.com>
2024-09-03 23:10:28 +0200

    Enemy is abstract class

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
     [SerializeField]
     private float m_speed = 10f;
