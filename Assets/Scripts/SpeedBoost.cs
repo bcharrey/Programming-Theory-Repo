51e282c014e72d327e51f8df905c424a5d62acfc
bcharrey <61541836+bcharrey@users.noreply.github.com>
2024-09-03 21:16:05 +0200

    speedBoost now affects player attack cycle duration

--- Assets/Scripts/SpeedBoost.cs
+++ Assets/Scripts/SpeedBoost.cs
@@ -6,14 +6,18 @@ public class SpeedBoost : PowerUp
 {
     [SerializeField]
     private float m_speedMultiplier = 1.1f;
+    [SerializeField]
+    private float m_attackCycleReduction = 0.9f;
 
     public override void BoostPlayer()
     {
         PlayerController.Instance.MoveSpeed *= m_speedMultiplier;
+        PlayerController.Instance.AttackCycleDuration *= m_attackCycleReduction;
     }
 
     public override void UnboostPlayer()
     {
         PlayerController.Instance.MoveSpeed /= m_speedMultiplier;
+        PlayerController.Instance.AttackCycleDuration /= m_attackCycleReduction;
     }
 }
