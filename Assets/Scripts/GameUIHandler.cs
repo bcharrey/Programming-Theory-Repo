d6131871e1e61a6c1e317cb0f230dcb22937f34d
bcharrey <61541836+bcharrey@users.noreply.github.com>
2024-09-05 13:25:38 +0200

    added restart and main menu buttons to gameover screen

---
+++ Assets/Scripts/GameUIHandler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameUIHandler : MonoBehaviour
+{
+    public void RestartScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
